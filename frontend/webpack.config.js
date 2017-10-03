8fb63a89896d2609f1b1e0878bdf52eac27af7eb
e.sirotkin <e.sirotkin@socialquantum.com>
2017-10-03 15:30:25 +0300

    init

---
+++ frontend/webpack.config.js
@@ -0,0 +1,88 @@
+var path = require('path')
+var webpack = require('webpack')
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: [
+        './src/main.js'
+    ],
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        publicPath: '/dist/',
+        filename: 'build.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: {
+                    loaders: {}
+                    // other vue-loader options go here
+                }
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                loader: 'url-loader',
+            }
+        ]
+    },
+    context: __dirname,
+    plugins: [
+        new CopyWebpackPlugin([
+            {
+                from: 'src/assets/',
+                to: path.resolve(__dirname, './dist', 'assets')
+            }
+        ])
+    ],
+    resolve: {
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js'
+        }
+    },
+    devServer: {
+        historyApiFallback: true,
+        noInfo: true
+    },
+    performance: {
+        hints: false
+    },
+    devtool: '#eval-source-map'
+};
+
+/*if (process.env.NODE_ENV === 'local') {
+    console.log('LOCAL')
+    module.exports.plugins.push(
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+    );
+    module.exports.entry.push('webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000');
+    module.exports.devtool = '#source-map';
+    console.log(module.exports)
+}*/
+if (process.env.NODE_ENV === 'production') {
+    module.exports.devtool = '#source-map'
+    // http://vue-loader.vuejs.org/en/workflow/production.html
+    module.exports.plugins = (module.exports.plugins || []).concat([
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: '"production"'
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            sourceMap: true,
+            compress: {
+                warnings: false
+            }
+        }),
+        new webpack.LoaderOptionsPlugin({
+            minimize: true
+        })
+    ])
+}
---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+
+class Server {
+    constructor() {
+        this.app = express();
+        this.setup();
+        this.initEnv();
+        this.createServer();
+    }
+
+    setup() {
+        this.app.use(bodyParser.json());
+        this.app.use(bodyParser.urlencoded({extended: false}));
+        this.app.use(express.static(__dirname + '/dist'));
+    }
+
+    initEnv() {
+        try {
+            process.env.NODE_ENV = process.env.NODE_ENV? process.env.NODE_ENV : 'dev';
+            console.log('Init env', process.env.NODE_ENV)
+
+            //if (process.env.NODE_ENV == 'dev')
+                //this.createDevSettings();
+        } catch (err) {
+            console.log("Error when initialized env", {err});
+            process.exit();
+        }
+    }
+
+    createDevSettings() {
+        const webpack = require('webpack');
+        const webpackConfig = require('./webpack.config');
+        const webpackDevMiddleware = require('webpack-dev-middleware');
+        const webpackHotMiddleware = require('webpack-hot-middleware');
+        const compiler = webpack(webpackConfig);
+
+        this.app.use(webpackDevMiddleware(
+            compiler, {
+                noInfo: true,
+                publicPath: webpackConfig.output.publicPath
+            })
+        );
+        this.app.use(webpackHotMiddleware(compiler, {
+            log: console.log, path: '/__webpack_hmr', heartbeat: 10 * 1000
+        }));
+    }
+
+    createServer() {
+        const port = process.env.PORT || 8080;
+        this.server = require('http').createServer(this.app);
+        this.server.listen(port, () =>
+            console.log(`Start listening on localhost:${port}`)
+        );
+    }
+}
+
+module.exports = new Server;
